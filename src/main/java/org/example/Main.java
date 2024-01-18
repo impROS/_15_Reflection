d02345a49429172f25d0ea6317c32c6fb4dcc882
impROS <impROS@hotmail.com>
2024-01-19 01:32:11 +0300

    sync

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/*
+ * reflection, bir sınıfın veya nesnenin yapısal bilgilerine (alanlar, metotlar, arayüzler vb.)
+ *  dinamik olarak erişmeyi sağlayan bir mekanizmadır.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SampleClass sampleObject = new SampleClass();
+
+        Class<?> classObj = sampleObject.getClass();
+        System.out.println("Sınıf Adı: " + classObj.getName());
+
+        Field[] fields = classObj.getDeclaredFields();
+        System.out.println("\nSınıf Alanları:");
+        for (Field field : fields) {
+            System.out.println("Alan Adı: " + field.getName() + ", Türü: " + field.getType());
+        }
+
+        Method[] methods = classObj.getDeclaredMethods();
+        System.out.println("\nSınıf Metotları:");
+        for (Method method : methods) {
+            System.out.println("Metot Adı: " + method.getName() + ", Dönüş Türü: " + method.getReturnType());
+
+//invoke methodu ile private metotlara erişebiliriz.
+            if (method.getName().equals("privateMethod")) {
+                try {
+                    method.setAccessible(true);//private metotlara erişebilmek için yoksa =>
+                    // example.Main cannot access a member of class org.example.SampleClass with modifiers "privat
+                    // hatası alırdık
+                    method.invoke(sampleObject);//private metot çağrıldı.
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- src/main/java/org/example/SampleClass.java
+++ src/main/java/org/example/SampleClass.java
@@ -1,2 +1,14 @@
-package org.example;public class SampleClass {
+package org.example;
+
+public class SampleClass {
+    public String publicField;
+    private int privateField;
+
+    private void privateMethod() {
+        System.out.println("Özel metot çağrıldı.");
+    }
+
+    public void publicMethod() {
+        System.out.println("Genel metot çağrıldı.");
+    }
 }
